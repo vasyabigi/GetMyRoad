93ade7d7c1592623432fc4ad96d7c612608c7f1c
Anton Agafonov <equeny@fb.com>
2012-11-30 13:53:16 -0800

    Javscript date time fixes

--- GetMyRoad/static/js/views/app.js
+++ GetMyRoad/static/js/views/app.js
@@ -27,12 +27,12 @@ define([
         this.$('.timepicker').datetimepicker();
         var date = new Date();
         this.$('#fromTime').val(
-            date.getMonth() + '/' + date.getDay() + '/' + date.getFullYear() +
+            (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear() +
             ' ' + date.getHours() + ':' + date.getMinutes()
         );
         date.setHours(date.getHours() + 8);
         this.$('#toTime').val(
-            date.getMonth() + '/' + date.getDay() + '/' + date.getFullYear() +
+            (date.getMonth()) + 1 + '/' + date.getDate() + '/' + date.getFullYear() +
             ' ' + date.getHours() + ':' + date.getMinutes()
         );
     }
