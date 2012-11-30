4689e05f684c99dff403e5e4e72053f6bacbc947
Edifear <edifear@gmail.com>
2012-11-30 02:00:08 +0200

    added geolocation

--- GetMyRoad/static/js/views/map.js
+++ GetMyRoad/static/js/views/map.js
@@ -11,6 +11,8 @@ define([
         el: "#map",
 
         initialize: function() {
+            var self = this;
+
             var map = L.map('map', {
                 center: [50.451, 30.523],
                 zoom: 13
@@ -19,6 +21,25 @@ define([
             L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
                 maxZoom: 18
             }).addTo(map);
+
+            self.getMyLocation(map);
+        },
+
+        getMyLocation: function(map) {
+            map.locate({setView: true, maxZoom: 15});
+
+            map.on('locationfound', function(data) {
+                var circleOptions = {
+                    color: 'green',
+                    fillColor: 'green',
+                    fillOpacity: 0.5
+                };
+                L.circle(data.latlng, data.accuracy, circleOptions).addTo(map);
+                L.marker(data.latlng).bindPopup("You are here").addTo(map);
+            });
+            map.on('locationerror', function() {
+                console.log('Enable to find your location')
+            });
         }
 
   });
